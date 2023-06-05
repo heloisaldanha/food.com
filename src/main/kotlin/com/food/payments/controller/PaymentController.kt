a24cfe02c44aa7b5101e84eb0d8cc3b2f46a68c5
heloisaldanha <heloisa.saldanha@gmail.com>
2023-06-05 11:37:33 -0300

    creating the link between order-ms and payment-ms

--- src/main/kotlin/com/food/payments/controller/PaymentController.kt
+++ src/main/kotlin/com/food/payments/controller/PaymentController.kt
@@ -3,12 +3,14 @@ package com.food.payments.controller
 import com.food.payments.dto.PaymentDto
 import com.food.payments.service.PaymentService
 import jakarta.validation.Valid
+import jakarta.validation.constraints.NotNull
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.web.PageableDefault
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
@@ -56,6 +58,11 @@ class PaymentController(
         service.delete(id)
 
         return ResponseEntity.noContent().build()
+    }
+
+    @PatchMapping("/{id}/confirmar")
+    fun paymentConfirmed(@PathVariable @NotNull id: Long) {
+        service.paymentConfirmed(id)
     }
 
 }
--- src/main/kotlin/com/food/payments/service/PaymentService.kt
+++ src/main/kotlin/com/food/payments/service/PaymentService.kt
@@ -2,19 +2,24 @@ package com.food.payments.service
 
 import com.food.payments.dto.PaymentDto
 import com.food.payments.enum.Status
+import com.food.payments.http.OrderClient
 import com.food.payments.mapper.PaymentDtoMapper
 import com.food.payments.mapper.PaymentMapper
+import com.food.payments.model.Payment
 import com.food.payments.repository.PaymentRepository
+import jakarta.persistence.EntityNotFoundException
 import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
+import java.util.Optional
 
 @Service
 class PaymentService(
     private val repository: PaymentRepository,
     private val paymentDtoMapper: PaymentDtoMapper,
-    private val paymentMapper: PaymentMapper
+    private val paymentMapper: PaymentMapper,
+    private val order: OrderClient
 ) {
 
     fun getAllPayments(page: Pageable): Page<PaymentDto> {
@@ -39,7 +44,7 @@ class PaymentService(
     }
 
     fun update(id: Long, paymentDto: PaymentDto): PaymentDto {
-        var paymentById = repository.findById(id).orElseThrow { Exception("Pagamento não encontrado.") }
+        val paymentById = repository.findById(id).orElseThrow { Exception("Pagamento não encontrado.") }
         paymentById.paymentValue = paymentDto.value
         paymentById.name = paymentDto.name
         paymentById.number = paymentDto.number
@@ -57,4 +62,17 @@ class PaymentService(
         repository.findById(id).orElseThrow { Exception("Pagamento não encontrado.") }
         repository.deleteById(id)
     }
+
+    fun paymentConfirmed(id: Long) {
+        val payment: Optional<Payment> = repository.findById(id)
+        if (!payment.isPresent) {
+            throw EntityNotFoundException()
+        }
+
+        payment.get().status = Status.CONFIRMED
+        repository.save(payment.get())
+        order.putPayments(payment.get().idOrder)
+
+    }
+
 }
